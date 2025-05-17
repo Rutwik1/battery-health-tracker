07743e0ebc95ba970f03b122bad17984ef2f65c0
Rutwik <rutkalbandhe@gmail.com>
2025-05-17 20:14:42 +0530

    code updated

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // 👈 this is important
+      '@': path.resolve(__dirname, 'client/src'),  // point to client/src folder
     },
   },
 });
