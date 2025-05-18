0e56750af2a47f528fdcc3633a9a5a14133c95cb
Rutwik <rutkalbandhe@gmail.com>
2025-05-18 09:46:35 +0530

    new code executed

--- vite.config.ts
+++ vite.config.ts
@@ -45,38 +45,38 @@
 
 // ------------------
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-export default defineConfig({
-    root: './client',  // Set root folder here
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './client/src'),
-        },
-    },
-});
-
-// vite.config.ts
-// import { fileURLToPath } from 'url';
-// import { dirname, resolve } from 'path';
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+// import path from 'path';
 
 // export default defineConfig({
-//     root: './client',
+//     root: './client',  // Set root folder here
+//     plugins: [react()],
 //     resolve: {
 //         alias: {
-//             '@': resolve(__dirname, './client/src'),
+//             '@': path.resolve(__dirname, './client/src'),
 //         },
 //     },
-//     plugins: [react()],
 // });
+
+// vite.config.ts
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+    root: './client',
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './client/src'),
+        },
+    },
+    plugins: [react()],
+});
 
 
 // import { fileURLToPath } from 'url';
